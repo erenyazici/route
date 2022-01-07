37719ad8c1c8ffce0df74c4ebd60774036da95a5
Eren YAZICI <eren.yazici@hotmail.com>
2022-01-08 02:10:06 +0300

    New changelist

--- src/main/java/com/example/route/RouteApplication.java
+++ src/main/java/com/example/route/RouteApplication.java
@@ -1,15 +1,10 @@
 package com.example.route;
 
-import com.example.route.service.MapService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class RouteApplication {
-
-    private final MapService mapService;
 
     public static void main(String[] args) {
         SpringApplication.run(RouteApplication.class, args);
